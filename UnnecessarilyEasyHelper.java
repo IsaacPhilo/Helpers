a393289d96c74d8981d6b646b1acfb83517f8703
IsaacP <93623649+IsaacPhilo@users.noreply.github.com>
2021-11-03 19:06:21 -0500

    Add files via upload
    Paste these outside your main method to save time on printing.

---
+++ UnnecessarilyEasyHelper.java
@@ -0,0 +1,66 @@
+public class UnnecessarilyEasyHelper {
+	//Print, but in less code.
+	public static void p(String a) {
+		System.out.print(a);
+	}
+	public static void p(byte a) {
+		System.out.print(a);
+	}
+	public static void p(short a) {
+		System.out.print(a);
+	}
+	public static void p(int a) {
+		System.out.print(a);
+	}
+	public static void p(long a) {
+		System.out.print(a);
+	}
+	public static void p(float a) {
+		System.out.print(a);
+	}
+	public static void p(double a) {
+		System.out.print(a);
+	}
+	public static void p(boolean a) {
+		System.out.print(a);
+	}
+	public static void p(char a) {
+		System.out.print(a);
+	}
+	public static void p(Object a) {
+		System.out.print(a.toString());
+	}
+	
+	//Print Line, but in less code.
+	public static void pl(String a) {
+		System.out.println(a);
+	}
+	public static void pl(byte a) {
+		System.out.println(a);
+	}
+	public static void pl(short a) {
+		System.out.println(a);
+	}
+	public static void pl(int a) {
+		System.out.println(a);
+	}
+	public static void pl(long a) {
+		System.out.println(a);
+	}
+	public static void pl(float a) {
+		System.out.println(a);
+	}
+	public static void pl(double a) {
+		System.out.println(a);
+	}
+	public static void pl(boolean a) {
+		System.out.println(a);
+	}
+	public static void pl(char a) {
+		System.out.println(a);
+	}
+	public static void pl(Object a) {
+		System.out.println(a.toString());
+	}
+	
+}
